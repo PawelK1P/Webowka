5479ae0e6eb4c864c00d1ed2595ffcd9eb4d9d04
PanPawlik5 <90028065+PawelK1P@users.noreply.github.com>
2025-03-30 20:51:47 +0200

    Add files via upload

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,10 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react'; //useState zarządza komponentem
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import "../styles.css";
-// funkcje związane z działaniem kont zostanie opracowane wkrótce
+import { Link } from 'react-router-dom';
+
 function Account() {
+  // zapis formularza w komponencie
+  const [formData, setFormData] = useState({ // formData - zapisuje dane, setFormData - aktualizuje formData, useState - zapisuje początkowe wartości
+    email: "",
+    password: "",
+    role: "client",
+    loading: false //ustawienie wartości początkowej dla paska ładowania
+  });
+
+  // Obsługa zmiany wartości w formularzu:
+  const handleChange = (e) => { 
+    const { name, value } = e.target; 
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    })); 
+  };
+
+  // Obsługa wysyłania formularza: 
+  const handleSubmit = (event) => { 
+    event.preventDefault(); //zapobiega przeładowaniu strony po wysłaniu formularza
+    setFormData(prevState => ({
+      ...prevState,
+      loading: true,
+    })); // pokazanie paska ładowania
+
+    // Symulacja opóźnienia 
+    setTimeout(() => {
+      console.log(formData);
+      
+      // Po zakończeniu procesu ładowania, pokaż alert i zakończ ładowanie
+      setFormData(prevState => ({
+        ...prevState,
+        loading: false,
+      }));
+
+      // Wyświetlenie alertu po zakończeniu rejestracji
+      alert("Rejestracja zakończona pomyślnie!");
+    }, 2000);
+  };
   return (
-    <h1>Rejestracja</h1>
+    <>
+      <Navbar />
+      <div className="account">
+        <h1>Rejestracja</h1>
+        <form onSubmit={handleSubmit}>
+          <p>E-mail</p>
+          <input 
+            type="email" 
+            name="email" 
+            value={formData.email} 
+            onChange={handleChange} 
+            required 
+          />
+          <p>Hasło</p>
+          <input 
+            type="password" 
+            name="password" 
+            value={formData.password} 
+            onChange={handleChange} 
+            required 
+          />
+          
+          <p>Typ konta</p>
+          <select 
+            name="role" 
+            value={formData.role} 
+            onChange={handleChange}
+          >
+            <option value="client">Klient</option>
+            <option value="employee">Pracownik</option>
+          </select>
+          
+          <br /><br />
+          <input 
+            type="submit" 
+            value="Submit" 
+            disabled={formData.loading} 
+          />
+          
+          {formData.loading && (
+            <div style={{ marginTop: 5, fontWeight: "bold" }}>Ładowanie...</div>
+          )}
+        </form>
+        <Link to="/">Wróć na stronę główną</Link>
+      </div>
+      <Footer />
+    </>
   );
-}
+};
 
 export default Account;
